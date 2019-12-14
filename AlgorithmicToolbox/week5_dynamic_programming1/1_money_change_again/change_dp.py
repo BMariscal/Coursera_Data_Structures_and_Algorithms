7e1c4191b5d60377894f9d42f3eb0cfc1525ae59
Briceida <BriceidaMariscal@gmail.com>
2019-12-14 15:42:21 -0800

    DP

--- AlgorithmicToolbox/week5_dynamic_programming1/1_money_change_again/change_dp.py
+++ AlgorithmicToolbox/week5_dynamic_programming1/1_money_change_again/change_dp.py
@@ -2,8 +2,18 @@
 import sys
 
 def get_change(m):
-    #write your code here
-    return m // 4
+    coins = [1,3,4]
+    dp = [float('inf')] * (m+1)
+    dp[0] = 0
+
+    for coin in coins:
+        for i in range(coin, m+1):
+            dp[i] = min(dp[i], dp[i-coin]+ 1)
+
+    return dp[m]
+
+
+
 
 if __name__ == '__main__':
     m = int(sys.stdin.read())
